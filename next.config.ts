07c50ce1621c3b383b7bcebff953c29f4cac9ac2
Mihir Nagda <mihirnagda28@gmail.com>
2026-03-19 13:22:16 +0530

    fix something

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  trailingSlash: false,
   images: {
     unoptimized: true,
     formats: ["image/avif", "image/webp"],
