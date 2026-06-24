baacd926f1256638318ec9cfcb92aee3674a109c
Mihir Nagda <mihirnagda28@gmail.com>
2026-06-24 14:56:54 +0530

    add memanager to portfolio

--- lib/sectors.ts
+++ lib/sectors.ts
@@ -67,7 +67,7 @@ export interface SectorPageData {
 
 const allProjects = projectsData as PortfolioProject[];
 
-const PUBLIC_PROJECT_EXCLUSIONS = new Set(["alkem"]);
+const PUBLIC_PROJECT_EXCLUSIONS = new Set(["alkem", "memanager"]);
 
 const projectsBySlug = new Map(
     allProjects.map((project) => [project.slug, project]),
